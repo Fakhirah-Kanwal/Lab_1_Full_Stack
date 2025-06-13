1df86fa5d8323eb506b525f21f9f0a60bd031e82
Fakhirah Kanwal <fakhirahwaqas11@gmail.com>
2025-06-13 06:56:57 +0200

    Created files and folders
    connected the database and gitignore file to ignore the necessary files

---
+++ db.js
@@ -0,0 +1,30 @@
+
+//Import the mongoose library to interact with Mongo DB
+const mongoose = require('mongoose');
+
+//Import dotenv to load environment variables from env file
+const dotenv = require('dotenv');
+
+//Load the environment variables
+dotenv.config();
+
+//Asynchronise function to connect to MongoDB atlas
+const connectDB = async () => {
+    try{
+        //Try to connect using Connecion_url from .env file
+        await mongoose.connect(process.env.CONNECTION_URL);
+
+        //Success msg if connection successful
+        console.log('MongoDB Connection is established');
+
+    }catch (err){
+
+        //failed msg if connection fails
+        console.error('Mongo DB connection is failed', err.message);
+
+        //Exit the process if fails
+        process.exit(1);
+    }
+};
+// Call to the function when runs
+connectDB();
